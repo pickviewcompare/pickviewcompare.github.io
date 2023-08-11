38f944ec1801d795906ddda607af0738fdcf83f3
username-matthew <matthew@olk1.com>
2023-08-11 18:28:00 +0100

    update branding and featured artist names

---
+++ README.md
@@ -0,0 +1,9 @@
+## Pick View Compare
+### 12 Artists rescued from the void
+
+(LIVE SITE)[https://pickviewcompare.github.io]
+
+![preview](/preview.png)
+
+
+`PVC :>0`
--- js/header.js
+++ js/header.js
@@ -1,6 +1,6 @@
 // header animated titles loop
 const headerElement = document.getElementById('rolling-header');
-    const strings = ['SOCiETY iS $!cK Y0', 'social cred!t card reader', 'mvm', '30words'];
+    const strings = ['12 Artists rescued from the void', 'featuring...', 'dehzer', '/|\\ v /|\\', 'social cred!t card reader', 'terminal glitch', 'digital rips', 'smush', 'marmalade malaise', 'bills boards', 'geogen', 'toot', 'CG', 'S!$Y', 'PVC'];
     let currentHeaderIndex = 0;
 
     function updateHeader() {
