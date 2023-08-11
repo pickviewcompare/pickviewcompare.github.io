6c3a3cb770f2df7c20619400842c8cf72577b1db
username-matthew <matthew@olk1.com>
2023-08-11 14:55:57 +0100

    refactor to modular sytem based on json file data populating the home grid and each home grid corresponding modal's array of images

--- js/homeGrid.js
+++ js/homeGrid.js
@@ -1,53 +1,117 @@
-  const homeGridImageUrls = [
-    '../images/home-grid/pushchair.jpg',
-    '../images/home-grid/billboards.jpg',
-    '../images/home-grid/collage.jpg',
-    '../images/home-grid/easycss.jpg',
-    '../images/home-grid/geogen.jpg',
-    '../images/home-grid/oldnewz-kick.gif',
-    '../images/home-grid/iart.jpg',
-    '../images/home-grid/presetloop.jpg',
-    '../images/home-grid/procreate.jpg',
-    '../images/home-grid/stories.jpg',
-    '../images/home-grid/vertical.jpg',
-    '../images/home-grid/dehaze.jpg',
-  ];
+const modal = document.getElementById('imageModal');
+const modalContent = document.querySelector('.modal-content');
+const carousel = document.getElementById('imageCarousel');
 
-function generateGrid() {
+async function generateGrid() {
   const homeGrid = document.getElementById('homeGrid');
 
-  homeGridImageUrls.forEach((imageUrl) => {
-    const divElement = document.createElement('div');
-    divElement.classList.add('grid-item');
+  try {
+    const response = await fetch('../images/home-grid/homeGrid.json');
+    const imageUrls = await response.json();
 
-    const imgElement = document.createElement('img');
-    imgElement.src = imageUrl;
-    imgElement.alt = 'Image';
+    imageUrls.forEach((imageUrl) => {
+      const divElement = document.createElement('div');
+      divElement.classList.add('grid-item');
 
-    divElement.appendChild(imgElement);
-    homeGrid.appendChild(divElement);
+      const imgElement = document.createElement('img');
+      imgElement.src = imageUrl;
+      imgElement.alt = 'Image';
 
-    const imgElements = document.querySelectorAll('img');
-    const socials = document.querySelector('.socials')
-    
-      imgElements.forEach((img) => {
-        if(!socials.contains(img)){
-          img.addEventListener('click', () => {
-          openModal();
-        });
-      }
+      // Create a click event listener for each image
+      imgElement.addEventListener('click', async () => {
+        await openModal(imageUrl);
+        await createImageElements(carousel, imageUrl); // Pass carousel and imageUrl
+      });
+
+      divElement.appendChild(imgElement);
+      homeGrid.appendChild(divElement);
     });
-  });
-}
-
-function openModal() {
-  modal.style.display = 'flex';
-  document.body.style.overflow = 'hidden';
+  } catch (error) {
+    console.error('Error fetching image URLs:', error);
+  }
 }
 
 function closeModal() {
   modal.style.display = 'none';
   document.body.style.overflow = 'auto';
 }
+
+async function openModal(imageUrl) {
+  try {
+    // Get the image filename without the extension
+    const imageName = imageUrl.split('/').pop().replace('.jpg', '');
+
+    // Construct the JSON file URL based on the clicked image's filename
+    const jsonUrl = `../images/${imageName}/${imageName}.json`; // Adjust the path as needed
+
+    const response = await fetch(jsonUrl);
+    const imageUrls = await response.json();
+
+    // Remove the clicked image from the array
+    const filteredImageUrls = imageUrls.filter(url => url !== imageUrl);
+
+    // Clear existing content
+    modalContent.innerHTML = '';
+
+    modal.style.display = 'flex';
+
+    // Create a new carousel element for the modal
+    const newCarousel = document.createElement('div');
+    newCarousel.id = 'imageCarousel';
+    newCarousel.classList.add('carousel'); // Add the carousel class
+
+    // Append the clicked image to the carousel
+    const clickedImgElement = document.createElement('img');
+    clickedImgElement.src = imageUrl;
+    clickedImgElement.alt = 'Image';
+    newCarousel.appendChild(clickedImgElement);
+
+    filteredImageUrls.forEach((url) => {
+      const img = document.createElement('img');
+      img.src = url;
+      img.alt = 'Image';
+      newCarousel.appendChild(img);
+    });
+
+    modalContent.appendChild(newCarousel);
+  } catch (error) {
+    console.error('Error fetching image URLs:', error);
+  }
+}
+
+
+
+async function createImageElements(carousel, imageUrl) {
+  try {
+    // Get the image filename without the extension
+    const imageName = imageUrl.split('/').pop().replace('.jpg', '');
+
+    // Construct the JSON file URL based on the clicked image's filename
+    const jsonUrl = `../images/${imageName}/${imageName}.json`; // Adjust the path as needed
+
+    const response = await fetch(jsonUrl);
+    const imageUrls = await response.json();
+
+    // Clear existing content
+    carousel.innerHTML = '';
+
+    imageUrls.forEach((url) => {
+      const img = document.createElement('img');
+      img.src = url;
+      img.alt = 'Image';
+      carousel.appendChild(img);
+    });
+
+    // Adjust the width of the carousel based on the number of images
+    // Assuming each image is 100% wide
+    const totalWidth = imageUrls.length * 100; 
+    carousel.style.width = `${totalWidth}%`;
+  } catch (error) {
+    console.error('Error fetching image URLs:', error);
+  }
+}
+
+
+modalContent.addEventListener('click', closeModal);
 
 generateGrid();
